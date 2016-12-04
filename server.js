a9a06ba3bb3cc3e5ee5c95d03272fc4c1cdbf340
Kathryn Nutzman <kathryn.nutzman@gmail.com>
2016-12-03 19:26:59 -0600

    worked on session

--- server.js
+++ server.js
@@ -1,55 +1,98 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
-var session = require('express-session');
+var expressSession = require('express-session');
 var port = process.env.PORT || 9003;
 
+var compression = require('compression');
+var session = require('cookie-session');
 
-var app = module.exports = express();
-app.use(bodyParser.json());
-app.use(cors());
-app.use(express.static(__dirname + '/public'));
+var errors = require('./errors');
+var logs = require('./logs');
 
-var massive = require('massive');
-// var config = require('./config.js');
 var connectionString = process.env.HEROKU_POSTGRESQL_COBALT_URL;
 
-var stripe = require('stripe')("sk_test_UZkqBkIphlq8nrvz39Hnp4vG"); //secretkey
+module.exports = function Web(app) {
+	var web = express();
+  var errs = errors(bool(false);
+	
 
-app.get('/paysuccess', function(req, res){
-	res.render('paysuccess', {
+	  // Express configuration
+	web
+	  .set('view engine', 'jade')
+	  .set('view cache', true);
 
-	});
-});
-
-app.post('/charge', function(req, res){
-	var token = req.body.stripeToken;
-	var chargeAmount = req.body.chargeAmount;
-	var charge = stripe.charges.create({
-		amount: chargeAmount,
-		source: token
-	}, function(err, charge){
-		if(err & err.type === "StripeCardError"){
-			console.log("Your card was declined");
-		}
-	});
-	console.log("Your payment was successful");
-	res.redirect('/paysuccess');
-
-});
-
-app.use(session({
-	secret: process.env.SESSION_SECRET,
-	saveUninitialized: true,
-	resave: false,
-	cookie: { secure: true }
-}));
+	  // Shared middleware
+	web
+	  .use(compression())
+		.use(express.static(__dirname + '/public'));
+		.use(bodyParser.json())
+	  .use(bodyParser.urlencoded({ extended: true }))
+	  .use(session({ secret: process.env.SESSION_SECRET, maxAge: ONE_WEEK }));
 
 
-var  massiveInstance = massive.connectSync({connectionString : connectionString})
+	  // Shared error handling
+	  web
+	    .use(errs.notFound)
+	    .use(errs.log)
+	    .use(errs.json)
+	    .use(errs.html);
 
-app.set('db', massiveInstance);
-var controller = require('./controller');
+	  return web;
+	};
+
+
+
+
+
+
+
+
+
+// app.use(bodyParser.json());
+// app.use(cors());
+// app.use(express.static(__dirname + '/public'));
+//
+// var massive = require('massive');
+// // var config = require('./config.js');
+// var connectionString = process.env.HEROKU_POSTGRESQL_COBALT_URL;
+//
+// var stripe = require('stripe')("sk_test_UZkqBkIphlq8nrvz39Hnp4vG"); //secretkey
+//
+// app.get('/paysuccess', function(req, res){
+// 	res.render('paysuccess', {
+//
+// 	});
+// });
+//
+// app.post('/charge', function(req, res){
+// 	var token = req.body.stripeToken;
+// 	var chargeAmount = req.body.chargeAmount;
+// 	var charge = stripe.charges.create({
+// 		amount: chargeAmount,
+// 		source: token
+// 	}, function(err, charge){
+// 		if(err & err.type === "StripeCardError"){
+// 			console.log("Your card was declined");
+// 		}
+// 	});
+// 	console.log("Your payment was successful");
+// 	res.redirect('/paysuccess');
+//
+// });
+//
+// app.use(session({
+// 	secret: process.env.SESSION_SECRET,
+// 	saveUninitialized: true,
+// 	resave: false,
+// 	cookie: { secure: true }
+// }));
+//
+//
+// var  massiveInstance = massive.connectSync({connectionString : connectionString})
+//
+// app.set('db', massiveInstance);
+// var controller = require('./controller');
 
 
 
@@ -74,7 +117,7 @@ app.get('/api/order/display', controller.getOrder);
 
 
 
-app.listen(port, function(){
+web.listen(port, function(){
   console.log("Successfully listening on", port)
 })
 	console.log('server controller');
